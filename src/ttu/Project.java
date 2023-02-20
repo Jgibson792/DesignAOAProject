32eeb7e57dea6d9de570fe1009aba8a4b27d8749
Jgibson792 <125433992+Jgibson792@users.noreply.github.com>
2023-02-20 16:10:08 -0600

    Update Project.java
    Added BubbleSort and Rank classes. Added method calls to main.

--- src/ttu/Project.java
+++ src/ttu/Project.java
@@ -76,11 +76,11 @@ public class Project {
 		}
 		
 		
+		//Comment out sections to try certain sorting algorithms
 		
-		
-		MergeSort M1 = new MergeSort();
-		M1.sort(s1rearranged, 0, s1rearranged.length-1);
-		System.out.println(M1.getInversions());
+		MergeSort M1 = new MergeSort();				//Creates MergeSort object
+		M1.sort(s1rearranged, 0, s1rearranged.length-1);	//Sorts the array and counts inversions
+		System.out.println(M1.getInversions());			//Displays the # of inversions
 		MergeSort M2 = new MergeSort();
 		M2.sort(s2rearranged, 0, s2rearranged.length-1);
 		System.out.println(M2.getInversions());
@@ -93,6 +93,9 @@ public class Project {
 		MergeSort M5 = new MergeSort();
 		M5.sort(s5rearranged, 0, s5rearranged.length-1);
 		System.out.println(M5.getInversions());
+		
+		//Displays the rank of the source file based on its reliability(# of inversions)
+		Rank.sort(M1.getInversions(), M2.getInversions(), M3.getInversions(), M4.getInversions(), M5.getInversions());
 		
 		QuickSort Q1 = new QuickSort();
 		Q1.sort(s1rearranged, 0, s1rearranged.length-1);
@@ -109,6 +112,26 @@ public class Project {
 		QuickSort Q5 = new QuickSort();
 		Q5.sort(s5rearranged, 0, s5rearranged.length-1);
 		System.out.println(Q5.getInversions());
+		
+		Rank.sort(Q1.getInversions(), Q2.getInversions(), Q3.getInversions(), Q4.getInversions(), Q5.getInversions());
+		
+		BubbleSort B1 = new BubbleSort();
+		B1.sort(s1rearranged, s1rearranged.length);
+		System.out.println(B1.getInversions());
+		BubbleSort B2 = new BubbleSort();
+		B2.sort(s2rearranged, s2rearranged.length);
+		System.out.println(B2.getInversions());
+		BubbleSort B3 = new BubbleSort();
+		B3.sort(s3rearranged, s3rearranged.length);
+		System.out.println(B3.getInversions());
+		BubbleSort B4 = new BubbleSort();
+		B4.sort(s4rearranged, s4rearranged.length);
+		System.out.println(B4.getInversions());
+		BubbleSort B5 = new BubbleSort();
+		B5.sort(s5rearranged, s5rearranged.length);
+		System.out.println(B5.getInversions());
+		
+		Rank.sort(B1.getInversions(), B2.getInversions(), B3.getInversions(), B4.getInversions(), B5.getInversions());
 		
 	}
 	
@@ -230,6 +253,98 @@ class MergeSort {
     }
 }
 
+ class QuickSort {
+    private int countInversions = 0;
+    
+    public int getInversions() {
+    	return countInversions;
+    }
+    
+    public void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            sort(arr, low, pivotIndex - 1);
+            sort(arr, pivotIndex + 1, high);
+        }
+    }
 
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+                countInversions += j - i;
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+
+class BubbleSort {
+	//Getter for inversions
+	public int getInversions() {
+    	return inversions;
+    }
+	
+	private int inversions = 0;
+	
+	//BubbleSort method
+	void sort(int arr[], int n) {
+		//Traverse through the array
+		for (int i = 0; i < n - 1; i++) {
+			for(int j = 0; j < n - i -1;j++) {
+				//If data at j is greater than data at j+1, swap
+				if(arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+					//Every swap is an inversion in BubbleSort
+					inversions++;
+				}
+			}
+		}
+	}
+}
+
+static class Rank {
+		
+	//This method adds all inverse counts to an array and rearranged them in ascending order
+	public static void sort(int inv1, int inv2, int inv3, int inv4, int inv5) {
+		//Create array with inverse counts
+		int[] arr = new int[]{inv1, inv2, inv3, inv4, inv5};
+		//Create array with source file numbers
+		int[] sourceList = new int[] {1, 2, 3, 4, 5};
+		
+		//Selection sort to order inverse counts in ascending order
+		for(int i = 0;i<arr.length;i++) {
+			for(int j = i+1;j<arr.length;j++) {
+				if(arr[j]<arr[i]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+					//If an inverse count is reordered, so is the corresponding source file
+					temp = sourceList[j];
+					sourceList[j] = sourceList[i];
+					sourceList[i] = temp;
+				}
+			}
+		}
+		//Prints the rank of the source files from most reliable to least reliable
+		System.out.println("Ranked");
+		for(int k=0;k<5;k++) {
+			System.out.println("Source " + sourceList[k]);
+		}
+	}
+}
 
 
